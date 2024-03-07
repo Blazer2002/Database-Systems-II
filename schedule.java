402a3190428ab8be2e9f3fb40c3d6694e950ca38
Blazer2002 <ivanchan202@gmail.com>
2024-03-07 10:27:41 -0500

    Create schedule.java

---
+++ schedule.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Schedule{
+
+    public static void main(String args[]){
+
+        private ArrayList<String> t1 = new ArrayList<String>();
+        private ArrayList<String> t2 = new ArrayList<String>();
+        private ArrayList<String> t3 = new ArrayList<String>();
+        private int m1 = 0;
+        private int m2 = 0;
+        private int m3 = 0;
+        private ArrayList<ArrayList<String>> transactions = new ArrayList<ArrayList<String>>();
+
+        /* for(int i=0; i< number of operations; i++)
+         * for(int i=0; i<3; i++)
+         * {
+         *      String s;
+         *      ArrayList<String> t = transactions.get(i);
+         *      if t == t1, then 
+         *          s = t.get(m1);
+         *          m1++;
+         *      if t == t2, then 
+         *          s = t.get(m2);
+         *          m2++;
+         *      if t == t3, then 
+         *          s = t.get(m3);
+         *          m3++;
+         *      add(s);
+         * }
+         */
+    }
+}
